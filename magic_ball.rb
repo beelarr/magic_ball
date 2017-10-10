8a0abbe2b265e19ef0b81b99d48a97fdd0cdc5c4
beelarr <bryonl@me.com>
2017-10-10 10:19:09 -0500

    Before refactor

--- magic_ball.rb
+++ magic_ball.rb
@@ -1,7 +1,9 @@
 class Magicball
-  ANSWERS = ["Outcome does not look likely", "Not now", "Better luck next time", "Great job"]
+  ANSWERS = ['Outcome does not look likely', 'Not now', 'Better luck next time', 'Great job'].freeze
   def ask(question)
-    55
+    # ANSWERS.sample # sample pulls a random sample from the array
+    raise 'Question has invalid format.' unless
+      question.is_a?(String) && question[-1] == '?'
+    ANSWERS.sample
   end
-
 end
--- magic_ball_test.rb
+++ magic_ball_test.rb
@@ -4,8 +4,10 @@ require_relative 'magic_ball'
 class MagicballTest < MiniTest::Test
   def test_ask_returns_an_answer
     magicball = Magicball.new
-    assert !magicball.ask("Test").nil?
+    # assert !magicball.ask("Test").nil?
     # assert magicball.ask("Test") != nil
+    assert_includes Magicball::ANSWERS,
+    magicball.ask("Test")
   end
   def test_predefined_answers_is_array
     assert_kind_of Array, Magicball::ANSWERS
@@ -13,5 +15,10 @@ class MagicballTest < MiniTest::Test
   def test_predefined_answers_is_not_empty
     refute_empty Magicball::ANSWERS
   end
-
+  def test_raises_error_when_question_is_number
+    assert_raises "Questions has invalid format." do
+    magicball = Magicball.new
+    magicball.ask(1)
+  end
+  end
 end
